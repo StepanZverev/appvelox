72c5536e33f79a8169196395f9ea0d723ade4b92
Stepan Zverev <stepanzverev@gmail.com>
2020-10-10 13:54:58 +0300

    build

---
+++ build/precache-manifest.868c605f44f01626b3c515de81a86e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bd8ae907b6a1f21f0048db931fef66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "268ddab36ed07a80a4e3",
+    "url": "/static/css/main.28c5aa68.chunk.css"
+  },
+  {
+    "revision": "1ce3f88be5daded1f84e",
+    "url": "/static/js/2.7f2dfa3b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7f2dfa3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268ddab36ed07a80a4e3",
+    "url": "/static/js/main.d5529222.chunk.js"
+  },
+  {
+    "revision": "16ce7de746dce3b71341",
+    "url": "/static/js/runtime-main.a7d59ddc.js"
+  }
+]);
